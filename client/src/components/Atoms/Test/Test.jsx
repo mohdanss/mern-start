0fa0b996233e7ba723addc5b4d4dcd2e108fcc56
Muhammad Ans <anstig3r@gmail.com>
2023-06-02 08:04:21 +0500

    basic client server MERN app is ready!

---
+++ client/src/components/Atoms/Test/Test.jsx
@@ -0,0 +1,31 @@
+import React, { useEffect, useState } from "react";
+import { checkBackendConnection } from "../../../utils/utils";
+
+const Test = () => {
+  const [backendConnected, setBackendConnected] = useState(false);
+  const [message, setMessage] = useState("waiting for backend connection...");
+
+  useEffect(() => {
+    checkBackendConnection()
+      .then((res) => {
+        const [flag, message] = res;
+        setBackendConnected(flag);
+        setMessage(message);
+      })
+      .catch((err) => {
+        console.error(err);
+        setMessage(err);
+        setBackendConnected(false);
+      });
+  }, []);
+
+  return (
+    <div>
+      <h1>Test</h1>
+      <p>Connection Status: {backendConnected ? "Connected" : "Not Connected"}</p>
+      <p>Message: {message}</p>
+    </div>
+  );
+};
+
+export default Test;
--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-const express = require('express');
-const session = require('express-session');
-const cors = require('cors')
-const dotenv = require('dotenv');
-const http = require('http');
-dotenv.config()
+const express = require("express");
+const session = require("express-session");
+const cors = require("cors");
+const dotenv = require("dotenv");
+const http = require("http");
+dotenv.config();
 
-const {log, ip} = require('./utils/utils');
-const connectToDatabase = require('./config/connection');
-const routes = require('./routes');
+const { log, ip } = require("./utils/utils");
+const connectToDatabase = require("./config/connection");
+const routes = require("./routes");
 
 connectToDatabase();
 
@@ -15,22 +15,23 @@ connectToDatabase();
 const app = express();
 const server = http.createServer(app);
 
-const PORT = process.env.PORT || 3001
+const PORT = process.env.PORT || 3001;
 
 // initializing the session
 const sess = {
-    secret: "Very secret secret",
-    cookie: {},
-    resave: false,
-    saveUninitialized: true,
-}
+	secret: "Very secret secret",
+	cookie: {},
+	resave: false,
+	saveUninitialized: true,
+};
 
 app.use(session(sess));
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(routes);
+
 app.use(cors());
 
 server.listen(PORT, () => {
-    log(`Server listening on port ${ip}:${PORT}`);
-})
+	log(`Server listening on port ${ip}:${PORT}`);
+});
