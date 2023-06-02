0febba527a702a193f629dafa2e85b5f1c507cda
Muhammad Ans <anstig3r@gmail.com>
2023-06-02 08:08:14 +0500

    a small styling xD

--- client/src/components/Atoms/Test/Test.jsx
+++ client/src/components/Atoms/Test/Test.jsx
@@ -2,30 +2,39 @@ import React, { useEffect, useState } from "react";
 import { checkBackendConnection } from "../../../utils/utils";
 
 const Test = () => {
-  const [backendConnected, setBackendConnected] = useState(false);
-  const [message, setMessage] = useState("waiting for backend connection...");
+	const [backendConnected, setBackendConnected] = useState(false);
+	const [message, setMessage] = useState("waiting for backend connection...");
 
-  useEffect(() => {
-    checkBackendConnection()
-      .then((res) => {
-        const [flag, message] = res;
-        setBackendConnected(flag);
-        setMessage(message);
-      })
-      .catch((err) => {
-        console.error(err);
-        setMessage(err);
-        setBackendConnected(false);
-      });
-  }, []);
+	useEffect(() => {
+		checkBackendConnection()
+			.then((res) => {
+				const [flag, message] = res;
+				setBackendConnected(flag);
+				setMessage(message);
+			})
+			.catch((err) => {
+				console.error(err);
+				setMessage(err);
+				setBackendConnected(false);
+			});
+	}, []);
 
-  return (
-    <div>
-      <h1>Test</h1>
-      <p>Connection Status: {backendConnected ? "Connected" : "Not Connected"}</p>
-      <p>Message: {message}</p>
-    </div>
-  );
+	return (
+		<div style={styles.center}>
+			<h1>Test</h1>
+			<p>
+				Connection Status: {backendConnected ? "Connected" : "Not Connected"}
+			</p>
+			<p>Message: {message}</p>
+		</div>
+	);
+};
+
+const styles = {
+	center: {
+		textAlign: "center",
+        color: "red",
+	},
 };
 
 export default Test;
